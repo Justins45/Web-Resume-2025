dae1053d813bf7cecbfaf7bd67912f7c0b66b201
Justin Shaw <shaw.justin@shaw.ca>
2025-03-21 20:28:04 -0600

    add initial about content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,26 @@ import Container from '@/components/Container.vue'
   <div class="bg-black min-h-screen text-white">
     <Container>
       <!-- TESTER FOR WIDTH -->
-      <div class="h-2 bg-white"></div>
+      <div class="h-2 bg-white mb-5"></div>
       <!-- header -->
       <div></div>
       <!-- about -->
-      <div></div>
+      <div>
+        <div>
+          <h2 class="uppercase text-xs">About</h2>
+          <div class="space-y-3">
+            <p class="">
+              An upcoming professional from Calgary Alberta. Have experience with VueJS, Tailwind,
+              C#, and much more. Currently looking to supply users with services that will bow their
+              socks off.
+            </p>
+            <p>
+              Was in the workforce for 2 years, company sold, now looking to get back in with an
+              improved skill set and a drive for building better apps.
+            </p>
+          </div>
+        </div>
+      </div>
       <!-- experience -->
       <div></div>
       <!-- skills  ?? cool cards? -->
